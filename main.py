14f8d699198828e2d1a06dd1165de3e959eb7342
Nikita <orlov.nickita@gmail.com>
2021-12-17 22:23:56 +0300

    Part 1. Start from Telegram-Bot greeting

---
+++ main.py
@@ -0,0 +1,46 @@
+import telebot
+
+bot = telebot.TeleBot('5097875553:AAGR3Mt8vaLxy6ny8n7uGSLQy_AUUFGOltM')
+
+
+@bot.message_handler(commands = ['start'])
+def send_welcome(message):
+    """
+    Функция, которая реагирует на команду /start при запуске бота
+    :param message: В качестве параметра передается сообщение из чата
+    :return: Возвращается приветствие
+    :rtype str
+    """
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!\n'
+                                      f'Я Бот от туристического агентства Too Easy Travel!\nЯ помогу тебе '
+                                      'выбрать место для отпуска по отличным ценам')
+
+
+@bot.message_handler(commands = ['hello_world'])
+def send_welcome_to_the_world(message):
+    """
+    Функция, которая реагирует на команду /hello_world при запуске бота
+    :param message: В качестве параметра передается сообщение из чата
+    :return: Возвращается приветствие
+    :rtype str
+    """
+    bot.send_message(message.chat.id, 'Привет всему миру!')
+
+
+@bot.message_handler(content_types = ['text'])
+def greeting(message):
+    """
+    Функция, которая реагирует на приветствие пользователя чата
+    :param message: В качестве параметра передается сообщение из чата
+    :return: Возвращается приветствие
+    :rtype str
+
+    """
+    if message.text in ["Привет", "Привет!", "привет", "привет!"]:
+        bot.send_message(message.chat.id, 'И тебе привет, мой друг!')
+    else:
+        bot.send_message(message.chat.id, 'Такого я еще не понимаю')
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
